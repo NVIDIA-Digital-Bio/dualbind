b62f40f9783e1a9fcfe3ca22c07842fda09ad7e8
mengliu-nvidia <154927895+mengliu-nvidia@users.noreply.github.com>
2025-07-29 14:17:51 -0700

    Remove commented dependency versions

--- env.sh
+++ env.sh
@@ -7,9 +7,9 @@ conda activate dualbind
 # Install key dependencies
 conda install pytorch==1.13.0 torchvision==0.14.0 torchaudio==0.13.0 pytorch-cuda=11.7 -c pytorch -c nvidia
 conda install lightning -c conda-forge
-conda install -c conda-forge rdkit # 2023.09.05
-pip install biopython # 1.79
-pip install chemprop # 1.6.1
+conda install -c conda-forge rdkit
+pip install biopython
+pip install chemprop
 pip install hydra-core
 pip install omegaconf
 pip install biotite
